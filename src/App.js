5ad99d26d38189e15fb78ec75d78a10c62a92f61
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-08-19 11:29:13 -0300

    Importação de módulos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react"
-import { SafeAreaView } from "react-native"
+import { SafeAreaView, Text } from "react-native"
 
 import Primeiro from './components/Primeiro'
+import Default, { Comp1,Comp2 } from './components/Multi'
 
-export default () => <SafeAreaView><Primeiro></Primeiro></SafeAreaView>
+export default () => (
+    <SafeAreaView>
+        <Default />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro />
+    </SafeAreaView>
+)

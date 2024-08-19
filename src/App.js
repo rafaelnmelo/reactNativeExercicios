d01156ed455bfb62c530650f0ae6db8acb022223
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-08-19 12:44:58 -0300

    Melhorando acessibilidade com estilos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react"
-import { SafeAreaView, Text } from "react-native"
+import { SafeAreaView, StyleSheet } from "react-native"
 
 import Primeiro from './components/Primeiro'
 import Default, { Comp1,Comp2 } from './components/Multi'
 
 export default () => (
-    <SafeAreaView>
+    <SafeAreaView style={style.App}>
         <Default />
         <Comp1 />
         <Comp2 />
         <Primeiro />
     </SafeAreaView>
 )
+
+const style = StyleSheet.create({
+    App: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
--- src/components/Multi.js
+++ src/components/Multi.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { SafeAreaView, Text } from "react-native"
+import { Text } from 'react-native'
+import Estilo from './estilo'
 
-export default function Comp() {
-    return <Text>Comp #Oficial</Text>
+function Comp() {
+    return <Text style={Estilo.fonteGrande}>Comp #Oficial</Text>
 }
 
-export function Comp1() {
-    return <Text>Comp #01</Text>
+function Comp1() {
+    return <Text style={Estilo.fonteGrande}>Comp #01</Text>
 }
 
-export function Comp2() {
-    return <Text>Comp #02</Text>
+function Comp2() {
+    return <Text style={Estilo.fonteGrande}>Comp #02</Text>
 }
+
+export { Comp1, Comp2 }
+export default Comp

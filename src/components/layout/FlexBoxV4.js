028a801f5cc9c334d5dfdb45f459202527cb8464
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-09-02 11:27:16 -0300

    Ajuste de comentários

--- src/components/layout/FlexBoxV4.js
+++ src/components/layout/FlexBoxV4.js
@@ -13,25 +13,21 @@ export default props => {
 
 const style = StyleSheet.create({
     FlexV4: {
-        // flexDirection: "row", //inverter o main axis
-        // height: 350,
         flexGrow: 1,
         width: 100,
-        // alignItems: "center", //alinhamento no cross axis 
-        // justifyContent: "space-evenly", //de acordo com o main axis 
         backgroundColor: '#000'
     },
     V0: {
         backgroundColor: '#36c9a7',
         flexGrow: 1,
-        height: 500
+        height: 500 //receberá o tamanho fixo
     },
     V1: {
         backgroundColor: '#ff801a',
-        flexGrow: 9
+        flexGrow: 9 //crescerá com porcentagem maior da area disponivel
     },
     V2: {
         backgroundColor: '#dd22c1',
-        flexGrow: 1
+        flexGrow: 1 //receberá a menor área
     }
 })
